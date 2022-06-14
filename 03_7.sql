52ae411be936a4fb6197bed91be87e5892b22ced
hyerim02 <phl0218@naver.com>
2022-06-15 00:22:08 +0900

    study

---
+++ 03_7.sql
@@ -0,0 +1,8 @@
+DELIMITER //
+CREATE PROCEDURE myProc()
+BEGIN
+   SELECT * FROM memberTBL WHERE memberName='당탕이' ;
+   SELECT * FROM productTBL WHERE productName='냉장고' ;
+END//
+DELIMITER ; 
+CALL myProc() ; 
