4954bf706d5561891218bd099e1424a8f6949f92
hyerim02 <phl0218@naver.com>
2022-08-06 12:59:21 +0900

    file

---
+++ ch6 file/limit-create table.sql
@@ -0,0 +1,11 @@
+USE sqldb;
+SELECT addr FROM usertbl;
+SELECT addr FROM usertbl ORDER BY addr;
+SELECT DISTINCT addr FROM usertbl;
+USE employees;
+SELECT emp_no, hire_date FROM employees ORDER BY hire_date ASC LIMIT 5;
+USE sqldb;
+CREATE TABLE buytbl2 (SELECT * FROM buytbl);
+SELECT * FROM buytbl2;
+CREATE TABLE buytbl3 (SELECT userID, prodName FROM buytbl);
+SELECT * FROM buytbl3;
