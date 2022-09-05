ddc4865815b84d791fb9bfaf5e4ce99753af6426
hyerim02 <phl0218@naver.com>
2022-09-06 00:32:13 +0900

    file

---
+++ ch7 file/시스템정보.sql
@@ -0,0 +1,9 @@
+SELECT USER(),DATABASE();
+USE sqldb;
+SELECT * FROM usertbl;
+SELECT FOUND_ROWS();
+USE sqldb;
+UPDATE buytbl SET price=price*2;
+SELECT ROW_COUNT();
+SELECT SLEEP(5);
+SELECT '5초후에 이게 보여요';
