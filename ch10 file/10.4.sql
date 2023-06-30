9db6630cdcc019a93d314eeb236229502463720c
hyerim02 <phl0218@naver.com>
2023-06-30 15:35:36 +0900

    file

---
+++ ch10 file/10.4.sql
@@ -0,0 +1,208 @@
+CREATE DATABASE IF NOT EXISTS testDB;
+USE testDB;
+CREATE TABLE IF NOT EXISTS testTbl (id INT, txt VARCHAR(10));
+INSERT INTO testTbl VALUES(1, '레드벨벳');
+INSERT INTO testTbl VALUES(2, '잇지');
+INSERT INTO testTbl VALUES(3, '블랙핑크');
+
+DROP TRIGGER IF EXISTS testTrg;
+DELIMITER // 
+CREATE TRIGGER testTrg  -- 트리거 이름
+    AFTER  DELETE -- 삭제후에 작동하도록 지정
+    ON testTbl -- 트리거를 부착할 테이블
+    FOR EACH ROW -- 각 행마다 적용시킴
+BEGIN
+	SET @msg = '가수 그룹이 삭제됨' ; -- 트리거 실행시 작동되는 코드들
+END // 
+DELIMITER ;
+
+SET @msg = '';
+INSERT INTO testTbl VALUES(4, '마마무');
+SELECT @msg;
+UPDATE testTbl SET txt = '블핑' WHERE id = 3;
+SELECT @msg;
+DELETE FROM testTbl WHERE id = 4;
+SELECT @msg;
+
+-- ///////////////////////////////////////////////////////////--
+USE sqlDB;
+DROP TABLE userTBL; -- 구매테이블은 실습에 필요없으므로 삭제.
+CREATE TABLE userTBL
+( userID  char(8) NOT NULL PRIMARY KEY, 
+  name    varchar(10) NOT NULL, 
+  birthYear   int NOT NULL,  
+  addr	  char(2) NOT NULL, 
+  mobile1	char(3), 
+  mobile2   char(8), 
+  height    smallint,  
+  mDate    date
+);
+INSERT INTO usertbl VALUES('LSG', '이승기', 1987, '서울', '011', '1111111', 182, '2008-8-8');
+INSERT INTO usertbl VALUES('KBS', '김범수', 1979, '경남', '011', '2222222', 173, '2012-4-4');
+INSERT INTO usertbl VALUES('KKH', '김경호', 1971, '전남', '019', '3333333', 177, '2007-7-7');
+INSERT INTO usertbl VALUES('JYP', '조용필', 1950, '경기', '011', '4444444', 166, '2009-4-4');
+INSERT INTO usertbl VALUES('SSK', '성시경', 1979, '서울', NULL  , NULL      , 186, '2013-12-12');
+INSERT INTO usertbl VALUES('LJB', '임재범', 1963, '서울', '016', '6666666', 182, '2009-9-9');
+INSERT INTO usertbl VALUES('YJS', '윤종신', 1969, '경남', NULL  , NULL      , 170, '2005-5-5');
+INSERT INTO usertbl VALUES('EJW', '은지원', 1972, '경북', '011', '8888888', 174, '2014-3-3');
+INSERT INTO usertbl VALUES('JKW', '조관우', 1965, '경기', '018', '9999999', 172, '2010-10-10');
+INSERT INTO usertbl VALUES('BBK', '바비킴', 1973, '서울', '010', '0000000', 176, '2013-5-5');
+
+DROP TABLE buyTBL; -- 구매테이블은 실습에 필요없으므로 삭제.
+CREATE TABLE backup_userTBL
+( userID  char(8) NOT NULL PRIMARY KEY, 
+  name    varchar(10) NOT NULL, 
+  birthYear   int NOT NULL,  
+  addr	  char(2) NOT NULL, 
+  mobile1	char(3), 
+  mobile2   char(8), 
+  height    smallint,  
+  mDate    date,
+  modType  char(2), -- 변경된 타입. '수정' 또는 '삭제'
+  modDate  date, -- 변경된 날짜
+  modUser  varchar(256) -- 변경한 사용자
+);
+
+DROP TRIGGER IF EXISTS backUserTbl_UpdateTrg;
+DELIMITER // 
+CREATE TRIGGER backUserTbl_UpdateTrg  -- 트리거 이름
+    AFTER UPDATE -- 변경 후에 작동하도록 지정
+    ON userTBL -- 트리거를 부착할 테이블
+    FOR EACH ROW 
+BEGIN
+    INSERT INTO backup_userTBL VALUES( OLD.userID, OLD.name, OLD.birthYear, 
+        OLD.addr, OLD.mobile1, OLD.mobile2, OLD.height, OLD.mDate, 
+        '수정', CURDATE(), CURRENT_USER() );
+END // 
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS backUserTbl_DeleteTrg;
+DELIMITER // 
+CREATE TRIGGER backUserTbl_DeleteTrg  -- 트리거 이름
+    AFTER DELETE -- 삭제 후에 작동하도록 지정
+    ON userTBL -- 트리거를 부착할 테이블
+    FOR EACH ROW 
+BEGIN
+    INSERT INTO backup_userTBL VALUES( OLD.userID, OLD.name, OLD.birthYear, 
+        OLD.addr, OLD.mobile1, OLD.mobile2, OLD.height, OLD.mDate, 
+        '삭제', CURDATE(), CURRENT_USER() );
+END // 
+DELIMITER ;
+
+
+UPDATE userTBL SET addr = '몽고' WHERE userID = 'JKW';
+DELETE FROM userTBL WHERE height >= 177;
+
+SELECT * FROM backup_userTBL;
+
+TRUNCATE TABLE userTBL;
+
+SELECT * FROM backup_userTBL;
+
+
+DROP TRIGGER IF EXISTS userTBL_InsertTrg;
+DELIMITER // 
+CREATE TRIGGER userTBL_InsertTrg  -- 트리거 이름
+    AFTER INSERT -- 입력 후에 작동하도록 지정
+    ON userTBL -- 트리거를 부착할 테이블
+    FOR EACH ROW 
+BEGIN
+    SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = '데이터의 입력을 시도했습니다. 귀하의 정보가 서버에 기록되었습니다.';
+END // 
+DELIMITER ;
+INSERT INTO userTBL VALUES('ABC', '에비씨', 1977, '서울', '011', '1111111', 181, '2019-12-25');
+
+-- ////////////////////////////////////////////////////////////////////////--
+
+USE sqlDB;
+DROP TRIGGER IF EXISTS userTBL_BeforeInsertTrg;
+DELIMITER // 
+CREATE TRIGGER userTBL_BeforeInsertTrg  -- 트리거 이름
+    BEFORE INSERT -- 입력 전에 작동하도록 지정
+    ON userTBL -- 트리거를 부착할 테이블
+    FOR EACH ROW 
+BEGIN
+    IF NEW.birthYear < 1900 THEN
+        SET NEW.birthYear = 0;
+    ELSEIF NEW.birthYear > YEAR(CURDATE()) THEN
+        SET NEW.birthYear = YEAR(CURDATE());
+    END IF;
+END // 
+DELIMITER ;
+
+INSERT INTO userTBL VALUES('AAA', '에이', 1877, '서울', '011', '1112222', 181, '2022-12-25');
+INSERT INTO userTBL VALUES('BBB', '비이', 2977, '경기', '011', '1113333', 171, '2019-3-25');
+SELECT * FROM userTBL;
+
+SHOW TRIGGERS FROM sqlDB;
+
+DROP TRIGGER userTBL_BeforeInsertTrg;
+
+
+-- //////////////////////////////////////////////////////////////////--
+
+DROP DATABASE IF EXISTS triggerDB;
+CREATE DATABASE IF NOT EXISTS triggerDB;
+
+USE triggerDB;
+CREATE TABLE orderTbl -- 구매 테이블
+	(orderNo INT AUTO_INCREMENT PRIMARY KEY, -- 구매 일련번호
+          userID VARCHAR(5), -- 구매한 회원아이디
+	 prodName VARCHAR(5), -- 구매한 물건
+	 orderamount INT );  -- 구매한 개수
+CREATE TABLE prodTbl -- 물품 테이블
+	( prodName VARCHAR(5), -- 물건 이름
+	  account INT ); -- 남은 물건수량
+CREATE TABLE deliverTbl -- 배송 테이블
+	( deliverNo  INT AUTO_INCREMENT PRIMARY KEY, -- 배송 일련번호
+	  prodName VARCHAR(5), -- 배송할 물건		  
+	  account INT UNIQUE); -- 배송할 물건개수
+
+INSERT INTO prodTbl VALUES('사과', 100);
+INSERT INTO prodTbl VALUES('배', 100);
+INSERT INTO prodTbl VALUES('귤', 100);
+
+-- 물품 테이블에서 개수를 감소시키는 트리거
+DROP TRIGGER IF EXISTS orderTrg;
+DELIMITER // 
+CREATE TRIGGER orderTrg  -- 트리거 이름
+    AFTER  INSERT 
+    ON orderTBL -- 트리거를 부착할 테이블
+    FOR EACH ROW 
+BEGIN
+    UPDATE prodTbl SET account = account - NEW.orderamount 
+        WHERE prodName = NEW.prodName ;
+END // 
+DELIMITER ;
+
+-- 배송테이블에 새 배송 건을 입력하는 트리거
+DROP TRIGGER IF EXISTS prodTrg;
+DELIMITER // 
+CREATE TRIGGER prodTrg  -- 트리거 이름
+    AFTER  UPDATE 
+    ON prodTBL -- 트리거를 부착할 테이블
+    FOR EACH ROW 
+BEGIN
+    DECLARE orderAmount INT;
+    -- 주문 개수 = (변경 전의 개수 - 변경 후의 개수)
+    SET orderAmount = OLD.account - NEW.account;
+    INSERT INTO deliverTbl(prodName, account)
+        VALUES(NEW.prodName, orderAmount);
+END // 
+DELIMITER ;
+
+INSERT INTO orderTbl VALUES (NULL,'JOHN', '배', 5);
+
+SELECT * FROM orderTbl;
+SELECT * FROM prodTbl;
+SELECT * FROM deliverTbl;
+
+ALTER TABLE deliverTBL CHANGE prodName productName VARCHAR(5);
+
+INSERT INTO orderTbl VALUES (NULL, 'DANG', '사과', 9);
+
+SELECT * FROM orderTbl;
+SELECT * FROM prodTbl;
+SELECT * FROM deliverTbl;
